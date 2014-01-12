61b5ec78e35a30c2131541f50302a07e2f44622c
Samuel Dominguez <samuel.dominguez.lorenzo@gmail.com>
2014-01-12 14:26:01 +0000

    nevermind, first commit.

--- emacs.el
+++ emacs.el
@@ -1,16 +1,23 @@
-; Samuel's emacs init file
+; how annoying are gnu brats' indentation?
+(setq c-default-style "linux")
+(setq cpp-default-style "linux")
 
-; set default C indentation style to the linux kernel one and set tab width to 8
-(setq c-default-style "linux"
-      c-basic-offset 8)
+; why would goto-line not be mapped to a key binding? C-x g seems OK.
+(global-set-key (kbd "C-x g") 'goto-line)
 
-; set default font to Terminus (default size is 10)
-(set-face-attribute 'default nil :font "Terminus" :height 100)
+; hippies rock expansions by a great margin.
+(global-set-key	(kbd "M-_") 'hippie-expand)
 
-; disable blinking cursor
-(blink-cursor-mode 0)
+; colors are cool, don't get me wrong, but I just can't read with a rainbow.
+(global-font-lock-mode 0)
 
-; color-theme is installed via 'emacs-goodies' package, so lets initialize and 
-; yield my (yet) favourite color theme
-(color-theme-initialize)
-(color-theme-arjen)
+; why is there an annoying menu bar at the top of my editor?
+(menu-bar-mode -1)
+
+; if --color=no was set this wouldn't be necessary but we cannot do that from
+; the init file and would have to call it every time we invoked emacs, and
+; that's a pain, so we have to manually set the colors for the mode-line and
+; change it everytime we change terminal colors, which may be a pain
+(custom-set-variables)
+(custom-set-faces
+ '(mode-line ((t (:background "black" :foreground "white" :box (:line-width -1 :style released-button))))))
