d4216468f790192cb24fce2b415fbbeb2c6d7828
Samuel Dominguez <samuel.dominguez.lorenzo@gmail.com>
2014-02-16 18:38:32 +0000

    removed stuff that was only useful when using emacs in a terminal.

--- emacs.el
+++ emacs.el
@@ -7,17 +7,3 @@
 
 ; hippies rock expansions by a great margin.
 (global-set-key	(kbd "M-_") 'hippie-expand)
-
-; colors are cool, don't get me wrong, but I just can't read with a rainbow.
-(global-font-lock-mode 0)
-
-; why is there an annoying menu bar at the top of my editor?
-(menu-bar-mode -1)
-
-; if --color=no was set this wouldn't be necessary but we cannot do that from
-; the init file and would have to call it every time we invoked emacs, and
-; that's a pain, so we have to manually set the colors for the mode-line and
-; change it everytime we change terminal colors, which may be a pain
-(custom-set-variables)
-(custom-set-faces
- '(mode-line ((t (:background "black" :foreground "white" :box (:line-width -1 :style released-button))))))
